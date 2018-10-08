23814931b85a343c04544cb75a55b7cfe4bb6ef9
Esmarra <miguel.maranha@gmail.com>
2018-10-08 18:48:43 +0100

    EX2 initial

--- Labwork#1/ex1v2.m
+++ Labwork#1/ex1v2.m
@@ -10,10 +10,10 @@ axis([-world_size world_size, -world_size world_size , -world_size world_size])
 xlabel('X')
 ylabel('Y')
 zlabel('Z')
-text(0,0,0, 'O')
+text(0,0,0,'O')
 w_size=900;
 set(gcf, 'Position', [960-w_size/2, 540-w_size/2, w_size, w_size])
-view(140, 30);
+view(130, 20);
 grid on
 hold on
 
@@ -93,9 +93,9 @@ function h = create_cubo(Pontos)
     X =Pontos(1,:);
 	Y =Pontos(2,:);
 	Z =Pontos(3,:);
-    
-    h(1)=fill3(X(1:4),Y(1:4),Z(1:4),'r'); % Front(x=1)
-    h(2)=fill3(X(5:8),Y(5:8),Z(5:8),'w'); % Back(x=0)
+    [l c]=size(Pontos);
+    h(1)=fill3(X(1:c/2),Y(1:c/2),Z(1:c/2),'r'); % Front(x=1)
+    h(2)=fill3(X(c/2+1:c),Y(c/2+1:c),Z(c/2+1:c),'w'); % Back(x=0)
     h(3)=fill3([X(1),X(2),X(6),X(5)],[Y(1),Y(2),Y(6),Y(5)],[Z(1),Z(2),Z(6),Z(5)],'y'); % Bottom(z=0)
     h(4)=fill3([X(2),X(6),X(7),X(3)],[Y(2),Y(6),Y(7),Y(3)],[Z(2),Z(6),Z(7),Z(3)],'g'); % Side1(y=1)
     h(5)=fill3([X(4),X(3),X(7),X(8)],[Y(4),Y(3),Y(7),Y(8)],[Z(4),Z(3),Z(7),Z(8)],'b'); % Top(z=1)
