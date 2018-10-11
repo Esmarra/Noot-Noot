30247db6e999c2839422f566df87e6ea866de6a6
Esmarra <miguel.maranha@gmail.com>
2018-10-11 13:34:55 +0100

    variables

--- Labwork#1/ex2.m
+++ Labwork#1/ex2.m
@@ -13,48 +13,61 @@ ylabel('Y')
 zlabel('Z')
 text(0,0,0, 'O')
 w_size=900;
-set(gcf, 'Position', [960-w_size/2, 540-w_size/2, w_size, w_size])
-view(140, 30);
+set(gcf, 'Position', [1440-w_size/2, 540-w_size/2, w_size, w_size])
+view(135, 50);
 grid on
 hold on
 
+hA=1;
+HB=1;
+hC=1;
+hB=1;
+d=1;
+
 A=[0 0 0 0 0 0 0 0 -2 -2 -2 -2 -2 -2 -2 -2
     0 0 1 1 2 2 3 3 0 0 1 1 2 2 3 3
-    0 1 1 2 2 1 1 0 0 1 1 2 2 1 1 0
+    0 hA hA hA+HB hA+HB hA hA 0 0 hA hA hA+HB hA+HB hA hA 0
     1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1];
 B=[0 0 0 0 0 0 0 0 -2 -2 -2 -2 -2 -2 -2 -2
     0 0 1 1 2 2 3 3 0 0 1 1 2 2 3 3
-    0 2 2 1 1 2 2 0 0 2 2 1 1 2 2 0
+    0 hB+HB hB+HB hB hB hB+HB hB+HB 0 0 hB+HB hB+HB hB hB hB+HB hB+HB 0
     1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1];
 C=[ 7 5 5 2 2 0 7 5 5 2 2 0
     0 0 0 0 0 0 -2 -2 -2 -2 -2 -2
-    0 4 1 1 4 0 0 4 1 1 4 0
+    0 hC+hB+HB+hA hC hC hC+hB+HB+hA 0 0 hC+hB+HB+hA hC hC hC+hB+HB+hA 0
     1 1 1 1 1 1 1 1 1 1 1 1];
-d=1;
+
 M=[ 3 3 5 4 4 1 1 0 2 2 3 3 5 4 4 1 1 0 2 2
-    -1 0 0 d+1 1 1 d+1 0 0 -1 -1 0 0 d+1 1 1 d+1 0 0 -1
-    0 0 0 0 0 0 0 0 0 0 1 1 1 1 1 1 1 1 1 1
+    1 0 0 -1-d -1 -1 -1-d 0 0 1 1 0 0 -1-d -1 -1 -1-d 0 0 1
+    1 1 1 1 1 1 1 1 1 1 0 0 0 0 0 0 0 0 0 0
     1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1];
 
-% M=[0 0 0 0 0 0 0 0 0 0 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1
-%     2 2 0 1 1 4 4 5 3 3 2 2 0 1 1 4 4 5 3 3
-%     0 2 2 4 3 3 4 2 2 0 0 2 2 4 3 3 4 2 2 0
-%     1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1];
-%hM=create_M(M);
-%hA=create_AB(A);
-hB=create_AB(B);
-%hC=create_C(C);
+% hM=create_M(M);
+% hA=create_AB(A);
+% hB=create_AB(B);
+% hC=create_C(C);
 
-WTM=[rot('X',180,'deg') [0 8 1+5]'%rotate this 180?
+yA=1;
+xB=4;
+yB=4;
+xC=1;
+zM=8;
+
+% Alfa-Z Beta-Y Gama-X
+WTM=[Rot_RPY(0,0,0,'deg') [0 8 5]'%Rotate 180x
     0 0 0 1];
 
-WTA=[eye(3,3) [0 2 0]'
+WTA=[eye(3) [0 yA 0]'
+    0 0 0 1];
+WTAe=[[-1 0 0
+    0 -1 0
+    0 0 1] [0 yA+1.5 0]'
     0 0 0 1];
 ATW=InvTransform(WTA);
-WTB=[rot('Z',-90,'deg') [4 4 0]'
+WTB=[rot('Z',-90,'deg') [xB yB 0]'
     0 0 0 1];
 BTW=InvTransform(WTB);
-WTC=[eye(3,3) [0 -d 0]'
+WTC=[eye(3,3) [xC -d 0]'
     0 0 0 1];
 
 T=[rot('Z',90,'deg') [1 1 0]'
@@ -67,27 +80,27 @@ WTCu=[eye(3) [4 4 0]'
     0 0 0 1];
 %CuTW=Transformacao_Inversa(WTCu);
 %h=create_cubo(Cu);
-hM1=create_M(WTM*M);
+hM1=create_M(WTM*M); 
 hA1=create_AB(WTA*A);
 hB1=create_AB(WTB*B);
 hC1=create_C(WTC*C);
-%alpha(hM1,.01); %fade
-alpha(hA1,.01); %fade
-alpha(hB1,.01); %fade
-alpha(hC1,.01); %fade
-
-h1=create_cubo(WTM*Cu);
-h2=create_cubo(WTA*Cu);
-h3=create_cubo(WTB*Cu);
-h4=create_cubo(WTC*Cu);
-
-TC=ctraj(WTM, WTB,a_inc);
-for i=1:a_inc
-    delete(hM1)
-    hM1=create_M(TC(:,:,i)*M);
-    pause(a_speed);
-end
-WTM1=TC(:,:,a_inc); %Store Current Pos
+% %alpha(hM1,.01); %fade
+% alpha(hA1,.01); %fade
+% alpha(hB1,.01); %fade
+% alpha(hC1,.01); %fade
+% 
+% h1=create_cubo(WTM*Cu,'b');
+% h2=create_cubo(WTAe*Cu,'r');
+% h3=create_cubo(WTB*Cu,'g');
+% h4=create_cubo(WTC*Cu,'k');
+% 
+% TC=ctraj(WTM, WTA,a_inc);
+% for i=1:a_inc
+%     delete(hM1)
+%     hM1=create_M(TC(:,:,i)*M);
+%     pause(a_speed);
+% end
+% WTM1=TC(:,:,a_inc); %Store Current Pos
 
 % T=[rot('Z',90,'deg') [0 0 5]'
 %     0 0 0 1];
@@ -96,27 +109,25 @@ WTM1=TC(:,:,a_inc); %Store Current Pos
 %     delete(hM1)
 %     delete(hA1)
 %     hM1=create_M(TC(:,:,i)*M);
-%     hA1=create_M(TC(:,:,i)*A);
+%     hA1=create_AB(TC(:,:,i)*A);
 %     pause(a_speed);
 % end
 
 
-function h = create_cubo(Pontos)
+function h = create_cubo(Pontos,color)
     X =Pontos(1,:);
 	Y =Pontos(2,:);
 	Z =Pontos(3,:);
     [l c]=size(Pontos);
-    h(1)=fill3(X(1:c/2),Y(1:c/2),Z(1:c/2),'k'); % Front(x=1)
-    h(2)=fill3(X(c/2+1:c),Y(c/2+1:c),Z(c/2+1:c),'k'); % Back(x=0)
-    h(3)=fill3([X(1),X(2),X(6),X(5)],[Y(1),Y(2),Y(6),Y(5)],[Z(1),Z(2),Z(6),Z(5)],'k'); % Bottom(z=0)
-    h(4)=fill3([X(2),X(6),X(7),X(3)],[Y(2),Y(6),Y(7),Y(3)],[Z(2),Z(6),Z(7),Z(3)],'k'); % Side1(y=1)
-    h(5)=fill3([X(4),X(3),X(7),X(8)],[Y(4),Y(3),Y(7),Y(8)],[Z(4),Z(3),Z(7),Z(8)],'k'); % Top(z=1)
-    h(6)=fill3([X(1),X(4),X(8),X(5)],[Y(1),Y(4),Y(8),Y(5)],[Z(1),Z(4),Z(8),Z(5)],'k'); % Side2(y=0)
+    h(1)=fill3(X(1:c/2),Y(1:c/2),Z(1:c/2),color); % Front(x=1)
+    h(2)=fill3(X(c/2+1:c),Y(c/2+1:c),Z(c/2+1:c),color); % Back(x=0)
+    h(3)=fill3([X(1),X(2),X(6),X(5)],[Y(1),Y(2),Y(6),Y(5)],[Z(1),Z(2),Z(6),Z(5)],color); % Bottom(z=0)
+    h(4)=fill3([X(2),X(6),X(7),X(3)],[Y(2),Y(6),Y(7),Y(3)],[Z(2),Z(6),Z(7),Z(3)],color); % Side1(y=1)
+    h(5)=fill3([X(4),X(3),X(7),X(8)],[Y(4),Y(3),Y(7),Y(8)],[Z(4),Z(3),Z(7),Z(8)],color); % Top(z=1)
+    h(6)=fill3([X(1),X(4),X(8),X(5)],[Y(1),Y(4),Y(8),Y(5)],[Z(1),Z(4),Z(8),Z(5)],color); % Side2(y=0)
     alpha(h,.1); %fade
     
 end
-
-
 function h = create_AB(Pontos)
     X =Pontos(1,:);
 	Y =Pontos(2,:);
@@ -143,9 +154,9 @@ function h = create_M(Pontos)
     h(3)=fill3([X(1),X(c/2),X(c),X(c/2+1)],[Y(1),Y(c/2),Y(c),Y(c/2+1)],[Z(1),Z(c/2),Z(c),Z(c/2+1)],'y'); % Bottom(z=0)
     h(4)=fill3([X(c/2),X(c/2-1),X(c-1),X(c)],[Y(c/2),Y(c/2-1),Y(c-1),Y(c)],[Z(c/2),Z(c/2-1),Z(c-1),Z(c)],'g'); % Side1(y=1)
     h(5)=fill3([X(c/2-1),X(c/2-2),X(c-2),X(c-1)],[Y(c/2-1),Y(c/2-2),Y(c-2),Y(c-1)],[Z(c/2-1),Z(c/2-2),Z(c-2),Z(c-1)],'b'); % Top(z=1)
-    h(6)=fill3([X(5),X(4),X(14), X(15)],[Y(5),Y(4),Y(14),Y(15)],[Z(5),Z(4),Z(14),Z(15)],'m'); 
-    h(7)=fill3([X(5),X(6),X(16), X(15)],[Y(5),Y(6),Y(16),Y(15)],[Z(5),Z(6),Z(16),Z(15)],'y');
-    h(8)=fill3([X(6),X(7),X(17), X(16)],[Y(6),Y(7),Y(17),Y(16)],[Z(6),Z(7),Z(17),Z(16)],'m');
+    h(6)=fill3([X(c/4),X(c/4-1),X(3*c/4-1), X(3*c/4)],[Y(c/4),Y(c/4-1),Y(3*c/4-1), Y(3*c/4)],[Z(c/4),Z(c/4-1),Z(3*c/4-1), Z(3*c/4)],'m'); 
+    h(7)=fill3([X(c/4),X(c/4+1),X(3*c/4+1), X(3*c/4)],[Y(c/4),Y(c/4+1),Y(3*c/4+1), Y(3*c/4)],[Z(c/4),Z(c/4+1),Z(3*c/4+1), Z(3*c/4)],'y');
+    h(8)=fill3([X(c/4+1),X(c/4+2),X(3*c/4+2), X(3*c/4+1)],[Y(c/4+1),Y(c/4+2),Y(3*c/4+2), Y(3*c/4+1)],[Z(c/4+1),Z(c/4+2),Z(3*c/4+2), Z(3*c/4+1)],'m');
     h(9)=fill3([X(7),X(8),X(18), X(17)],[Y(7),Y(8),Y(18),Y(17)],[Z(7),Z(8),Z(18),Z(17)],'g');
     h(10)=fill3([X(c/4-2),X(c/4-1),X(c-(c/4)-1),X(c-(c/4)-2)],[Y(c/4-2),Y(c/4-1),Y(c-(c/4)-1),Y(c-(c/4)-2)],[Z(c/4-2),Z(c/4-1),Z(c-(c/4)-1),Z(c-(c/4)-2)],'g'); % Top(z=1)
     h(11)=fill3([X(2),X(3),X(c/2+3),X(c/2+2)],[Y(2),Y(3),Y(c/2+3),Y(c/2+2)],[Z(2),Z(3),Z(c/2+3),Z(c/2+2)],'b'); % Top(z=1)
