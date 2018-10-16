b72ccbaf1fa64a0c2c0eb98884969fdd0c0a9a92
Esmarra <miguel.maranha@gmail.com>
2018-10-16 16:02:35 +0100

    muito bom

--- Labwork#1/ex3.m
+++ Labwork#1/ex3.m
@@ -16,12 +16,12 @@ view(135, 50);
 grid on
 hold on
 %%
-%    t d alpha  a offset   
-DH=[t1 0  pi/2  0      pi/2     %0->1
-    t2 0     0  4      0     %1->2
-    t3 0     0  2      0     %2->
-    t4 0 -pi/2  0  -pi/2     %3->4
-    t5 1     1  0      0];   %4->G
+%    t d a alfa offset   
+DH=[t1 0 0  pi/2  pi/2 %0->1
+    t2 0 4   0     0   %1->2
+    t3 0 2   0     0   %2->3
+    t4 0 0 -pi/2 -pi/2 %3->4
+    t5 1 0   0     0];   %4->G
 
 
 %% ==== Pos Home ==== %%
@@ -29,25 +29,33 @@ DH=[t1 0  pi/2  0      pi/2     %0->1
 
 %trplot();
 hold on;
-A=[ rot('X',90,'deg') [2 2 0]'
-    [0 0 0 1]];
-%H=trplot(A,'color','r');
+
 T=[0 0 0 0 0];
-%animate(5,L);
 DH=eval(subs(DH,[t1 t2 t3 t4 t5],deg2rad(T)));
 A=Arroz(DH);
 function A=Arroz(DH)
     [c,~]=size(DH);
-    cor=['r','b','g','m','y','w'];
+    cor=['r','b','g','m','y'];
     %A(:,:,c)=zeros(4,4,c);
     for i=1:c
         tet=DH(i,1);d=DH(i,2);a=DH(i,3);alf=DH(i,4);of=DH(i,5);
-        %eval(subs(DH,tet,90))
         A(:,:,i)=[cos(tet+of) -sin(tet+of)*cos(alf) sin(tet+of)*sin(alf) a*cos(tet+of)
             sin(tet+of) cos(tet+of)*cos(alf) -cos(tet+of)*sin(alf) a*sin(tet+of)
             0 sin(alf) cos(alf) d
             0 0 0 1];
-        trplot(A(:,:,i),'color',cor(i));
+        % Get Vector Pos
+        t(:,i)=A(1:3,4,i);
+        if(i<2)
+            T=A(:,:,i);
+        end
+        if(i>1)
+            T=T*A(:,:,i);
+        end
+        trplot(T,'color',cor(i));
+        
+        s=1; %vector size
+        %coordenadas do ponto anterior
+        %v=plot3([t(1)-1;t(1)-1+s],[t(2)-1;t(2)-1-s],[t(3),t(3)+s],'r'); % Vector
     end
 end
 %% ==== Animate Arm ==== %%
