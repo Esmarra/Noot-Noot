9d762aa895dd4f63ca37c6a01ad6f2274d1e5aaa
Esmarra <miguel.maranha@gmail.com>
2018-11-11 20:08:17 +0000

    v2

--- Labwork#2/ex2.m
+++ Labwork#2/ex2.m
@@ -6,6 +6,7 @@ a_speed=0.08;  %Animation Speed
 syms t1 t2 t3 t4 t5 t6; % Tetas
 syms d1 d2 d3 d4 d5 d6;
 syms a a1 a2 a3;
+syms l1;
 
 global Roboti Robotii Robotiii;
 %global d1 d2 d3 d4 d5 d6;
@@ -16,24 +17,24 @@ global a a1 a2 a3;
 DH_i=[ 0     d1     -pi/2    0      0      %0->1
        0     d2     pi/2     0      0];    %1->2
 
-L1i=Link('theta',0,'a',0,'alpha', -pi/2  , 'offset', 0, 'qlim', [0 5]);
-L2i=Link('theta',0,'a',0,'alpha', 0   , 'offset', 0, 'qlim', [0 5]);
+% L1i=Link('theta',0,'a',0,'alpha', -pi/2  , 'offset', 0, 'qlim', [0 5]);
+% L2i=Link('theta',0,'a',0,'alpha', 0   , 'offset', 0, 'qlim', [0 5]);
 
-Roboti=SerialLink([L1i L2i], 'name', 'BoT i)');
-Roboti.plot([0 0]);
-Roboti.teach([0 0]);
-
-a1=2;
-a2=2;
-d2=4;
+% Roboti=SerialLink([L1i L2i], 'name', 'BoT i)');
+% Roboti.plot([0 0]);
+% Roboti.teach([0 0]);
+% 
+% a1=2;
+% a2=2;
+% d2=4;
 figure(2);
 DH_ii=[ t1   0      pi/2    a2    pi/2      %0->1
         0    d2      0      0      a1 ];     %1->2
-L1ii=Link('d',0, 'a', a2 , 'alpha', pi/2  , 'offset', pi/2 ); %R
-L2ii=Link('theta', 0, 'a', 0 , 'alpha', 0  , 'offset', a1, 'qlim', [0 2]); %Prismatic qlim maxes d-travel
-Robotii=SerialLink([L1ii L2ii], 'name', 'BoT ii)');
-Robotii.plot([0 0]);
-Robotii.teach([0 0]);
+% L1ii=Link('d',0, 'a', a2 , 'alpha', pi/2  , 'offset', pi/2 ); %R
+% L2ii=Link('theta', 0, 'a', 0 , 'alpha', 0  , 'offset', a1, 'qlim', [0 2]); %Prismatic qlim maxes d-travel
+% Robotii=SerialLink([L1ii L2ii], 'name', 'BoT ii)');
+% Robotii.plot([0 0]);
+% Robotii.teach([0 0]);
 
 figure(3);
 world_size=8;
@@ -44,14 +45,14 @@ DH_iii=[ t1   0       pi/2      a2    pi/2    %0->1
         t3   0        pi/2      0      0      %2->I
         0    a3         0       0     pi/2];  %I->EE
 
-L1iii=Link('d',0, 'a', 0 , 'alpha', pi/2  , 'offset', pi/2 ); %R
-L2iii=Link('theta', 0, 'a', 0 , 'alpha', -pi/2  , 'offset', a1, 'qlim', [0 2]); %Prismatic qlim maxes d-travel
-L3iii=Link('d',0, 'a', 0 , 'alpha', pi/2  , 'offset', 0 ); %R
-L4iii=Link('theta', 0, 'a', 0 , 'alpha', 0  , 'offset', pi/2, 'qlim', [0 0]); %Prismatic qlim maxes d-travel
-
-Robotiii=SerialLink([L1iii L2iii L3iii L4iii], 'name', 'BoT iii)');
-Robotiii.plot([0 0 0 0]);
-Robotiii.teach([0 0 0 0]);
+% L1iii=Link('d',0, 'a', 0 , 'alpha', pi/2  , 'offset', pi/2 ); %R
+% L2iii=Link('theta', 0, 'a', 0 , 'alpha', -pi/2  , 'offset', a1, 'qlim', [0 2]); %Prismatic qlim maxes d-travel
+% L3iii=Link('d',0, 'a', 0 , 'alpha', pi/2  , 'offset', 0 ); %R
+% L4iii=Link('theta', 0, 'a', 0 , 'alpha', 0  , 'offset', pi/2, 'qlim', [0 0]); %Prismatic qlim maxes d-travel
+% 
+% Robotiii=SerialLink([L1iii L2iii L3iii L4iii], 'name', 'BoT iii)');
+% Robotiii.plot([0 0 0 0]);
+% Robotiii.teach([0 0 0 0]);
 
 %% ==== Com Punho Esferico acopolado ==== %%
 
@@ -68,11 +69,11 @@ DH_i_pe=[ 0   a1+d1    -pi/2      0    pi/2    %0->1
 %% iii)
 %          theta d       alpha      a   offset 
 DH_iii_pe=[ t1   0        pi/2      0    pi/2    %0->1
-            0    d2      -pi/2      a      0     %1->2
+            0   d2+l1    -pi/2      0      0     %1->2
             t3   0        pi/2      0      0     %2->3
-            t4   0       -pi/2      a2      0    %3->4
+            t4   a3      -pi/2      0      0    %3->4
             t5   0        pi/2      0      0     %4->5
-            t6   0         0        a1   pi/2];  %5->6
+            t6   a4        0        0   pi/2];  %5->6
 oAhiii_pe_symb=simplify(FK_MGD_DH(DH_iii_pe));
 
 
--- Labwork#2/helper.m
+++ Labwork#2/helper.m
@@ -1,21 +1,37 @@
 syms t1 t2 t3 t4 t5 t6; % Tetas
 syms d1 d2 d3 d4 d5 d6;
-syms a a1 a2 a3;
+syms a a1 a2 a3 a4;
 syms nx ny nz ax ay az sx sy sz tx ty tz;
+syms l1;
 
+
+% %        theta d       alpha      a   offset
+% DH_i_pe=[ 0   a1+d1    -pi/2      0    pi/2    %0->1
+%           0   a2+d2      0        0   -pi/2     %1->2
+%           t3   0       -pi/2      0      0     %2->3
+%           t4   0        pi/2      0      0     %3->4
+%           t5   a3         0        0   pi/2];  %4->5
+% disp(" 0TEE ");
+% T0EE=simplify(FK_MGD_DH(DH_i_pe))
+% 
+% %    theta     d       alpha    a   offset
+% DH=[ 0     a1+d1     -pi/2    0      -pi/2      %0->1
+%        0     a2+d2      0       0      -pi/2];    %1->2
+% disp(" 0T2 ");
+% T02=simplify(FK_MGD_DH(DH))
 
 %        theta d       alpha      a   offset
-DH_i_pe=[ 0   a1+d1    -pi/2      0    pi/2    %0->1
-          0   a2+d2      0        0   -pi/2     %1->2
+DH_i_pe=[ t1   0       pi/2      a2    pi/2    %0->1
+          0 l1+d2     0        0   0     %1->2
           t3   0       -pi/2      0      0     %2->3
           t4   0        pi/2      0      0     %3->4
-          t5   0         0        a3   pi/2];  %4->5
+          t5  a4         0        0   pi/2];  %4->5
 disp(" 0TEE ");
 T0EE=simplify(FK_MGD_DH(DH_i_pe))
 
 %    theta     d       alpha    a   offset
-DH=[ 0     a1+d1     -pi/2    0      -pi/2      %0->1
-       0     a2+d2      0       0      -pi/2];    %1->2
+DH=[ t1   0       pi/2      a2    pi/2    %0->1
+          0 a1+a3+d2     0        0   0];     %1->2
 disp(" 0T2 ");
 T02=simplify(FK_MGD_DH(DH))
 
